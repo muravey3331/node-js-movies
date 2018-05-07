59bf075f8709a22e9b2912544a71a750c36996a2
Pavel Bevz <pavlik143@gmail.com>
2018-05-07 16:51:22 +0300

    started fix actions

--- client/components/MovieAbout.js
+++ client/components/MovieAbout.js
@@ -1,27 +1,22 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import { Link } from 'react-router-3';
+import {Link} from 'react-router-3';
 
 //components
 import Header from './Header';
 
 //actions
-import { openMovie } from '../actions';
+import {openMovie} from '../actions';
 
 class MovieAbout extends Component {
     constructor(props) {
         super(props);
-
         this.onOpenMovie = this.props.onOpenMovie.bind(this);
         this.routeId = this.props.params.id;
-        this.movie = this.props.movie;
     }
 
-    async componentDidMount() {
-        let res = await this.onOpenMovie(this.routeId);
-
-        console.log(this.props.movie);
-        console.log(this.movie);
+    componentDidMount() {
+        this.onOpenMovie(this.routeId);
     }
 
     render() {
@@ -39,15 +34,15 @@ class MovieAbout extends Component {
                         <div className="flex">
                             <div className="page-about__image">
                                 <img
-                                    src={`${ this.movie.image || "http://marcroftmedical.com/wp-content/themes/marcroft/images/default-blog.jpg" }`}
+                                    src={`${ this.props.movie.image || "http://marcroftmedical.com/wp-content/themes/marcroft/images/default-blog.jpg" }`}
                                     className="movie__img"/>
                             </div>
                             <div className="page-about__description">
-                                <h2 className="movie__title">{this.movie.title}</h2>
+                                <h2 className="movie__title">{this.props.movie.title}</h2>
                                 <div className="flex row">
                                     <p className="description__key">Actors:</p>
                                     <ul className="movie__actors-list description__value">
-                                        {this.movie.actors.map((name, index) => (
+                                        {this.props.movie.actors.map((name, index) => (
                                             <li key={index} className="movie__actor-item">{name},</li>))}
                                     </ul>
                                 </div>
@@ -55,22 +50,23 @@ class MovieAbout extends Component {
                                     <p className="description__key">Rating:</p>
                                     <div className="description__value">
                                         <div className="empty-star">
-                                            <div className="entire-star" style={{width: `${this.movie.rate * 10}%`}} />
+                                            <div className="entire-star"
+                                                 style={{width: `${this.props.movie.rate * 10}%`}}/>
                                         </div>
-                                        {this.movie.rate} / 10
+                                        {this.props.movie.rate} / 10
                                     </div>
                                 </div>
                                 <div className="flex row">
                                     <p className="description__key">Format:</p>
-                                    <p className="description__value">{this.movie.format}</p>
+                                    <p className="description__value">{this.props.movie.format}</p>
                                 </div>
                                 <div className="flex row">
                                     <p className="description__key">Released year:</p>
-                                    <p className="description__value">{this.movie.year}</p>
+                                    <p className="description__value">{this.props.movie.year}</p>
                                 </div>
                                 <div className="flex row">
                                     <p className="description__key">Description:</p>
-                                    <p className="description__value">{this.movie.text}</p>
+                                    <p className="description__value">{this.props.movie.text}</p>
                                 </div>
                             </div>
                         </div>
